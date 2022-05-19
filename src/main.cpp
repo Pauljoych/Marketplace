d5f85eb869e099777e73cc6de3712e3e980cdb8f
pauljoych <luthfiquart@gmail.com>
2022-05-19 16:53:02 +0700

    remove output lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <lib/marketplace.h>
 #include <lib/userlogin.h>
 
@@ -6,4 +7,23 @@ using namespace std;
 
 int main()
 {
+  ofstream txt("invoice.txt");
+  txt << "+-----------------------------------------------+\n";
+  txt << "|\t\tBukti Pembayaran\t\t|\n";
+  txt << "+-----------------------------------------------+\n";
+  txt << "| Nama Pelanggan	: EKO\t\t\t|\n";
+  txt << "| Alamat tujuan 	: alamat\t\t|\n";
+  txt << "| Asuransi produk	: ya\t\t\t|\n";
+  txt << "| Ekspedisi		: JNE\t\t\t|\n";
+  txt << "+-----------------------------------------------+\n";
+  txt << "| Nama barang	: alpro\t\t\t\t|\n";
+  txt << "| Jumlah barang	: 0\t\t\t\t|\n";
+  txt << "+-----------------------------------------------+\n";
+  txt << "| Diskon	: Rp.0\t\t\t\t|\n";
+  txt << "| Total bayar	: p\t\t\t\t|\n";
+  txt << "+-----------------------------------------------+\n";
+  txt << "|\tHarap simpan bukti pembayaran\t\t|\n";
+  txt << "|\t\tTerimakasih\t\t\t|\n";
+  txt << "+-----------------------------------------------+\n";
+  txt.close();
 }
