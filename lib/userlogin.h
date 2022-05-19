4124de6acf2a151cab2e06ba2798ab5d16ca53d6
fahrulsanaky <101966278+fahrulsanaky@users.noreply.github.com>
2022-05-19 15:33:50 +0900

    Update userlogin.h

--- lib/userlogin.h
+++ lib/userlogin.h
@@ -4,5 +4,15 @@ using namespace std;
 class UserLogin
 {
 private:
+  string name;
+  int nim;
 public:
+  void login(){
+    cout << "=================\n";
+    cout << "User = ";
+    cin >> name;
+    cout << "=================\n";
+    cout << "Password = ";
+    cin >> nim;
+    cout << "=================\n";
 };
