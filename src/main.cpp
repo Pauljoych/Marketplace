313664c7f58b06f490cfb041fe36d07153bc578c
fahrulsanaky <101966278+fahrulsanaky@users.noreply.github.com>
2022-05-21 15:53:49 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,39 +8,39 @@ using namespace std;
 int main()
 {
   cout << "========================\n";
-	cout << "Input User Name = ";
-	getline(cin, tempUserName);
+  cout << "Input User Name = ";
+  getline(cin, tempUserName);
 	
-	cout << "========================\n";
-	cout << "Input Banyak Barang Yang Ingin Dibeli = ";
-	cin >> banyak;
-	for(int i=1; i>=banyak; i++){
-		cout << "========================\n";
-	  cout << "Input Nama Barang = ";
-	  cin >> itemName;
-	  cout << "Input Harga Barang = ";
-	  cin >> itemPrice;	
-	}
+  cout << "========================\n";
+  cout << "Input Banyak Barang Yang Ingin Dibeli = ";
+  cin >> banyak;
+  for(int i=1; i>=banyak; i++){
+      cout << "========================\n";
+      cout << "Input Nama Barang = ";
+      cin >> itemName;
+      cout << "Input Harga Barang = ";
+      cin >> itemPrice;	
+  }
 
-	cout << "========================\n";
-  	cout << "Kurir : " << endl;
-  	cout << "J&T" << endl;
-  	cout << "SiCepat" << endl;
-  	cout << "AnterAja" << endl;
-  	cout << "========================\n";
-  	cout << "Input Kurir = ";
-  	cin >> list;
-  	if(list==1){
-  		kurirList="J&T";
-	}
-	else if(list==2){
-		kurirList="SiCepat";
-	}
-	else if(list==3){
-		kurirList="AnterAja";
-	}else{
-		kurirList="-";
-	}
+  cout << "========================\n";
+  cout << "Kurir : " << endl;
+  cout << "J&T" << endl;
+  cout << "SiCepat" << endl;
+  cout << "AnterAja" << endl;
+  cout << "========================\n";
+  cout << "Input Kurir = ";
+  cin >> list;
+  if(list==1){
+  	kurirList="J&T";
+  }
+  else if(list==2){
+	kurirList="SiCepat";
+  }
+  else if(list==3){
+	kurirList="AnterAja";
+  }else{
+	kurirList="-";
+  }
   
   ofstream txt("invoice.txt");
   txt << "+-----------------------------------------------+\n";
